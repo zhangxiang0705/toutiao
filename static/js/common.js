8705b058a59d5b63b4460cf19c5d108a645062b8
zhangxiang0705 <66882961+zhangxiang0705@users.noreply.github.com>
2020-06-14 03:13:00 +0800

    Add files via upload

---
+++ static/js/common.js
@@ -0,0 +1,23 @@
+var $,layer;
+layui.use(['layer','element','jquery'],function(){
+	$ = layui.$;
+
+	//隐藏左侧导航
+	$(".hideMenu").click(function(){
+		if($(".topLevelMenus li.layui-this a").data("url")){
+			layer.msg("此栏目状态下左侧菜单不可展开");  //主要为了避免左侧显示的内容与顶部菜单不匹配
+			return false;
+		}
+		$(".layui-layout-admin").toggleClass("showMenu");
+	})
+	//手机设备的简单适配
+    $('.site-tree-mobile').on('click', function(){
+		$('body').addClass('site-mobile');
+	});
+    $('.site-mobile-shade').on('click', function(){
+		$('body').removeClass('site-mobile');
+	});
+
+
+});
+
